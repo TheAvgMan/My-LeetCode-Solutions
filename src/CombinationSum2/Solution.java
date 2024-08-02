29ba0fc7964863cc9788126c50658402b72bb2ac
Hazem Zaki <hazemzaki.work@gmail.com>
2024-08-02 11:53:42 +0300

    Document Combination Sum 2 Solution

--- src/CombinationSum2/Solution.java
+++ src/CombinationSum2/Solution.java
@@ -8,6 +8,7 @@ class Solution {
     List<List<Integer>> combinationSum2(int[] candidates, int target) {
         List<List<Integer>> solutions = new ArrayList<>();
 
+        // An EXTREMELY IMPORTANT STEP is this sorting of the candidates array before going into recursion.
         Arrays.sort(candidates);
 
         exhaustStates(new ArrayList<>(), 0, 0, solutions, candidates, target);
@@ -28,6 +29,23 @@ class Solution {
         }
 
         int previousCandidate = -1;
+
+        /*
+            In this version of combination sum, we cannot have a number from candidates array repeated in
+            the current sum, that is, the same number in that position taken into current sum several times.
+            But if a number is repeated in the candidates array itself then they account as different
+            candidates because they are in different positions in the candidates array, therefore each of them
+            can be taken into current sum.
+                Based on this description, that's why in the recursive call below, you see the
+            startCandidatesIndex as i+1.
+                Also, because the candidates array, in this version, can contain repeated numbers, so it is
+            a MUST to sort the candidates array before recursing on it. That's because it's faster and more
+            efficient to deal with the repeated numbers in the candidates array when it's sorted.
+                Due to all of this description, there needed to exist an efficient way in our code to prevent
+            the duplication of combinations in the solutions array. And that's why previous candidate variable
+            was introduced, to prevent adding again to current sum the same value that was in the previous
+            iteration in the same recursive layer.
+        */
 
         for (int i = startCandidatesIndex; i < candidates.length; i++) {
             if (candidates[i] == previousCandidate) continue;
