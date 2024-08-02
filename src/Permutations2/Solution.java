19e76fceb727a86f1a1091b2e1e9a6f68fb6fcac
Hazem Zaki <hazemzaki.work@gmail.com>
2024-08-02 12:31:06 +0300

    Document Permutations 2 Solution

--- src/Permutations2/Solution.java
+++ src/Permutations2/Solution.java
@@ -11,6 +11,7 @@ class Solution {
         int[] reservedIndices = new int[nums.length];
         Arrays.fill(reservedIndices, 0);
 
+        // As in combination sum 2 solution, this sorting step is EXTREMELY IMPORTANT
         Arrays.sort(nums);
 
         exhaustStates(new ArrayList<>(), reservedIndices, solutions, nums);
@@ -30,6 +31,22 @@ class Solution {
 
         for (int i = 0; i < nums.length; i++) {
             if (reservedIndices[i] == 1) continue;
+
+            /*
+                In this loop, we iterate over the whole nums array in each recursive layer to check what
+                values have been used (to skip them) and what are the free values (to include them in the
+                current list).
+                    Similar to combination sum 2 solution, in the condition below, we check if, not only
+                is the value reserved or not, but also if the value before it is the same. If it is the
+                same, then if the previous value is already used / reserved, then there's no problem with
+                taking the current value into the current list as well, because the previous value was
+                used in a previous recursive layer. But if the previous value is not used / reserved
+                then we skip the current value because it'll repeat the same exhaustion path of the
+                previous value.
+                    So, finally, if the current value is free and not the same as the previous value, then
+                we pass by the 2 continue statements and go right away to the backtracking portion of the
+                code below.
+            */
 
             if (i != 0)
                 if (nums[i] == nums[i-1] && reservedIndices[i-1] == 0) continue;
