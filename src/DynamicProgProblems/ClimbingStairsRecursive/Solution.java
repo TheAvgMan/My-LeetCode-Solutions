e68e376253f39beff159e054bd0b8fb25296e513
Hazem Zaki <hazemzaki.work@gmail.com>
2024-08-11 12:24:50 +0300

    Add Climbing Stairs Recursive Solution

---
+++ src/DynamicProgProblems/ClimbingStairsRecursive/Solution.java
@@ -0,0 +1,30 @@
+package DynamicProgProblems.ClimbingStairsRecursive;
+
+import java.util.Arrays;
+
+class Solution {
+    private int n;
+    private int[] memo;
+
+    int climbStairs(int n) {
+        if (n == 1 || n == 2 || n == 3) return n;
+
+        this.n = n;
+        memo = new int[this.n];
+
+        Arrays.fill(memo, -1);
+
+        return dpRecursive(0);
+    }
+
+    private int dpRecursive(int currentStair) {
+        if (currentStair == n) return 1;
+        if (currentStair > n) return 0;
+
+        if (memo[currentStair] == -1)
+            memo[currentStair] = dpRecursive(currentStair + 1) +
+                    dpRecursive(currentStair + 2);
+
+        return memo[currentStair];
+    }
+}
